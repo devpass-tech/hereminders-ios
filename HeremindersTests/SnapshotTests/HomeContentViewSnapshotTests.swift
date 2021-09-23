fad62f763c1d6e6f6bd4b3ac7bb8be31ec3c7d6a
joao <joao.camargo@gmail.com>
2021-09-23 20:28:53 -0300

    gravacao snapshot do zero / remocao da xib transformada em viewcode

--- HeremindersTests/SnapshotTests/HomeContentViewSnapshotTests.swift
+++ HeremindersTests/SnapshotTests/HomeContentViewSnapshotTests.swift
@@ -20,49 +20,8 @@ class HomeContentViewSnapshotTests: XCTestCase {
 
 
     func testHomeContentView() { //para gerar a primeira imagem
-        let dataController = DataController()
-        let locationController = LocationController()
-        let placeController = PlaceDataController(dataController: dataController)
-        
-        placeController.addPlace(withName: "Casa Branca",
-                                 latitude: 38.8977, longitude: 77.0365,
-                                 address: "1600 Pennsylvania Avenue NW, Washington, DC 20500, United States")
-        
-        let reminderController = ReminderDataController(dataController: dataController, placeController: placeController)
-
-        
-        //ReminderItemModel(description: "Jogar água na planta", event: 1),
-        reminderController.addReminder(withPlaceName: "Casa Branca", latitude: 38.8977,
-                                       longitude: 77.0365, description: "SnaoShotTest",
-                                       event: 1, radius: 2, notificationIdentifier: "Casa Branca Notif")
-        
         let sut = HomeView()
         assertSnapshot(matching: sut, as: Snapshotting.image(size: UIScreen.main.bounds.size), record: false)
     }
-    
-    
-//    func testHomeContentView() { //para gerar a primeira imagem
-//        let dataController = DataController()
-//        let locationController = LocationController()
-//        let placeController = PlaceDataController(dataController: dataController)
-//
-//        placeController.addPlace(withName: "Casa Branca",
-//                                 latitude: 38.8977, longitude: 77.0365,
-//                                 address: "1600 Pennsylvania Avenue NW, Washington, DC 20500, United States")
-//
-//        let reminderController = ReminderDataController(dataController: dataController, placeController: placeController)
-//
-//
-//        //ReminderItemModel(description: "Jogar água na planta", event: 1),
-//        reminderController.addReminder(withPlaceName: "Casa Branca", latitude: 38.8977,
-//                                       longitude: 77.0365, description: "SnaoShotTest",
-//                                       event: 1, radius: 2, notificationIdentifier: "Casa Branca Notif")
-//
-//        let sut = HomeViewController(viewModel:
-//                                        HomeViewModel(locationController: locationController,
-//                                                                      placeController: placeController,
-//                                                                      reminderController: reminderController))
-//
-//        assertSnapshot(matching: sut, as: Snapshotting.image(size: UIScreen.main.bounds.size), record: false)
-//    }
+
 }
